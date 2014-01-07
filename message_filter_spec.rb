526920b861d68f4b895bd6c0570e3dab7b264957
Suguru Odai <ourqwers@gmail.com>
2014-01-08 01:57:20 +0900

    Add test case to check including NG word

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,4 +4,8 @@ require 'rspec'
 require 'message_filter'
 
 describe MessageFilter do
+  it 'should detect message with NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to be_truthy
+  end
 end
