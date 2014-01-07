1065cae557eb58d59f8577e8ebc1e589a8a55613
Suguru Odai <ourqwers@gmail.com>
2014-01-08 02:39:14 +0900

    Describe inline

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,10 +4,7 @@ require 'rspec'
 require 'message_filter'
 
 describe MessageFilter, 'with argument "foo"' do
-  before do
-    @filter = MessageFilter.new('foo')
-  end
-
+  before { @filter = MessageFilter.new('foo') }
   it { expect(@filter).to be_detect('hello from foo') }
   it { expect(@filter).not_to be_detect('hello, world') }
 end
