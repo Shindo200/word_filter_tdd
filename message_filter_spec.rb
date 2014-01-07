3035950b44c72733461686a2babc4422d36a62fe
Suguru Odai <ourqwers@gmail.com>
2014-01-08 02:26:57 +0900

    Use before

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,12 +4,15 @@ require 'rspec'
 require 'message_filter'
 
 describe MessageFilter do
-  it 'should detect message with NG word' do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello from foo')).to be_truthy
+  before :each do
+    @filter = MessageFilter.new('foo')
   end
+
+  it 'should detect message with NG word' do
+    expect(@filter.detect?('hello from foo')).to be_truthy
+  end
+
   it 'should not detect message with NG word' do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello, world!')).to be_falsy
+    expect(@filter.detect?('hello, world!')).to be_falsy
   end
 end
