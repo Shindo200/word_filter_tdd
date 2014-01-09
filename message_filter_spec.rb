03e0e9720e4df90d2fec7a0f4a461b792d50b962
Suguru Odai <ourqwers@gmail.com>
2014-01-09 21:32:49 +0900

    Add test case with 2 arguments

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -8,3 +8,8 @@ describe MessageFilter, 'with argument "foo"' do
   it { expect(@filter).to be_detect('hello from foo') }
   it { expect(@filter).not_to be_detect('hello, world') }
 end
+
+describe MessageFilter, 'with argument "foo","bar"' do
+  before { @filter = MessageFilter.new('foo', 'bar') }
+  it { expect(@filter).to be_detect('hello from bar') }
+end
