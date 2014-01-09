fc524cd5b66fcf9f6f6d4b523ceaad6af2c6c4a9
Suguru Odai <ourqwers@gmail.com>
2014-01-09 21:44:23 +0900

    Use share examples

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -3,15 +3,18 @@ require 'rubygems'
 require 'rspec'
 require 'message_filter'
 
-describe MessageFilter, 'with argument "foo"' do
-  before { @filter = MessageFilter.new('foo') }
+share_examples_for 'MessageFilter with arggment "foo"' do
   it { expect(@filter).to be_detect('hello from foo') }
   it { expect(@filter).not_to be_detect('hello, world') }
+end
+
+describe MessageFilter, 'with argument "foo"' do
+  before { @filter = MessageFilter.new('foo') }
+  it_should_behave_like 'MessageFilter with arggment "foo"'
 end
 
 describe MessageFilter, 'with argument "foo","bar"' do
   before { @filter = MessageFilter.new('foo', 'bar') }
   it { expect(@filter).to be_detect('hello from bar') }
-  it { expect(@filter).to be_detect('hello from foo') }
-  it { expect(@filter).not_to be_detect('hello, world') }
+  it_should_behave_like 'MessageFilter with arggment "foo"'
 end
