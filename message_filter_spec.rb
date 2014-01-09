12579cc09ce3aa6676ff67118fb594800a7bab9f
Suguru Odai <ourqwers@gmail.com>
2014-01-09 21:47:08 +0900

    Nest within MessageFilter test cases

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -3,18 +3,20 @@ require 'rubygems'
 require 'rspec'
 require 'message_filter'
 
-share_examples_for 'MessageFilter with arggment "foo"' do
-  it { expect(@filter).to be_detect('hello from foo') }
-  it { expect(@filter).not_to be_detect('hello, world') }
-end
+describe MessageFilter do
+  share_examples_for 'MessageFilter with arggment "foo"' do
+    it { expect(@filter).to be_detect('hello from foo') }
+    it { expect(@filter).not_to be_detect('hello, world') }
+  end
 
-describe MessageFilter, 'with argument "foo"' do
-  before { @filter = MessageFilter.new('foo') }
-  it_should_behave_like 'MessageFilter with arggment "foo"'
-end
+  describe 'with argument "foo"' do
+    before { @filter = MessageFilter.new('foo') }
+    it_should_behave_like 'MessageFilter with arggment "foo"'
+  end
 
-describe MessageFilter, 'with argument "foo","bar"' do
-  before { @filter = MessageFilter.new('foo', 'bar') }
-  it { expect(@filter).to be_detect('hello from bar') }
-  it_should_behave_like 'MessageFilter with arggment "foo"'
+  describe 'with argument "foo","bar"' do
+    before { @filter = MessageFilter.new('foo', 'bar') }
+    it { expect(@filter).to be_detect('hello from bar') }
+    it_should_behave_like 'MessageFilter with arggment "foo"'
+  end
 end
